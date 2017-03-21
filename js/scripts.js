332205bbe5c3b0f6c14791b873b20a569357ee6d
Dominic Brown and Ken Rutan <ken@>
2017-03-21 09:41:53 -0700

    add slideToggles to play with

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,18 @@ $(document).ready(function() {
 
 
   $(".clickable1").click(function() {
-    $(".walrus-showing").fadeOut();
-    $(".walrus-hidden").fadeIn();
+    $(".walrus-showing").slideToggle(1000);
+    $(".walrus-hidden").fadeToggle(4000);
   });
 
   $(".clickable2").click(function() {
-    $(".walrus-hidden").fadeOut();
-    $(".walrus-third").fadeIn();
+    $(".walrus-hidden").slideToggle(1000);
+    $(".walrus-third").slideToggle(1000);
   });
 
   $(".clickable3").click(function() {
-    $(".walrus-third").fadeOut();
-    $(".walrus-showing").fadeIn();
+    $(".walrus-third").slideToggle(1000);
+    $(".walrus-showing").slideToggle(1000);
   });
 
 
